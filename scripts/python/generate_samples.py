84dcd439cc48338aea127be314475726d94ebbe9
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2023-05-18 10:58:19 +0530

    Print unicode properly

--- scripts/python/generate_samples.py
+++ scripts/python/generate_samples.py
@@ -9,6 +9,6 @@ for usx_path in glob.glob(usx_samples_path):
     usx = etree.parse(usx_path)
     dict_out = usx_to_json.usx_to_json(usx.getroot())
     with open(outpath, 'w', encoding='utf-8') as outfile:
-        outfile.write(json.dumps(dict_out, indent=2))
+        outfile.write(json.dumps(dict_out, indent=2, ensure_ascii=False))
 
 
