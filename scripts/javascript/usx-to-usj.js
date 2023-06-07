9e1ffeee2a42544501abcd3bdc9f10c7d134042a
Mark Howe <mvahowe@gmail.com>
2023-06-07 11:47:00 +0200

    Lint

--- scripts/javascript/usx-to-usj.js
+++ scripts/javascript/usx-to-usj.js
@@ -1,20 +1,20 @@
 const { ArgumentParser } = require('argparse');
-const fs = require('fs');
+const path = require('path');
+const fse = require('fs-extra');
 const { DOMParser } = require('xmldom');
-const XMLSerializer = require('xmldom').XMLSerializer;
 
 const VERSION_NUM = '0.0.1-alpha.2';
 const SPEC_NAME = 'USJ';
 
-function convert_usx(input_usx_elmt) {
-  let key = input_usx_elmt.tagName;
+function usxDomToJson1(inputUsxElement) {
+  let key = inputUsxElement.tagName;
   let text = null;
-  let out_obj = {};
+  let outObj = {};
   let action = 'append';
   let attribs = {};
-  if (input_usx_elmt.attributes) {
-    for (let i = 0; i < input_usx_elmt.attributes.length; i++) {
-      let attrib = input_usx_elmt.attributes[i];
+  if (inputUsxElement.attributes) {
+    for (let i = 0; i < inputUsxElement.attributes.length; i++) {
+      let attrib = inputUsxElement.attributes[i];
       attribs[attrib.name] = attrib.value;
     }
   }
@@ -24,32 +24,32 @@ function convert_usx(input_usx_elmt) {
     delete attribs['style'];
   }
 
-  out_obj['type'] = key;
-  out_obj = { ...out_obj, ...attribs };
+  outObj['type'] = key;
+  outObj = { ...outObj, ...attribs };
 
-  if (input_usx_elmt.firstChild && input_usx_elmt.firstChild.nodeType === 3 && input_usx_elmt.firstChild.nodeValue.trim() !== '') {
-    text = input_usx_elmt.firstChild.nodeValue.trim();
+  if (inputUsxElement.firstChild && inputUsxElement.firstChild.nodeType === 3 && inputUsxElement.firstChild.nodeValue.trim() !== '') {
+    text = inputUsxElement.firstChild.nodeValue.trim();
   }
 
-  let children = Array.from(input_usx_elmt.childNodes);
-  out_obj['content'] = [];
+  let children = Array.from(inputUsxElement.childNodes);
+  outObj['content'] = [];
 
   if (text) {
-    out_obj['content'].push(text);
+    outObj['content'].push(text);
   }
 
   for (let child of children) {
-    if (child.tagName == undefined) {
+    if (child.tagName === undefined) {
       continue;
     }
-    let [child_dict, what_to_do] = convert_usx(child);
+    let [childDict, whatToDo] = usxDomToJson1(child);
 
-    switch (what_to_do) {
+    switch (whatToDo) {
       case 'append':
-        out_obj['content'].push(child_dict);
+        outObj['content'].push(childDict);
         break;
       case 'merge':
-        out_obj['content'] = out_obj['content'].concat(child_dict);
+        outObj['content'] = outObj['content'].concat(childDict);
         break;
       case 'ignore':
         break;
@@ -58,83 +58,83 @@ function convert_usx(input_usx_elmt) {
     }
 
     if (child.nextSibling && child.nextSibling.nodeType === 3 && child.nextSibling.nodeValue.trim() !== '') {
-      out_obj['content'].push(child.nextSibling.nodeValue.trim());
+      outObj['content'].push(child.nextSibling.nodeValue.trim());
     }
   }
 
-  if (out_obj['content'].length === 0) {
-    delete out_obj['content'];
+  if (outObj['content'].length === 0) {
+    delete outObj['content'];
   }
 
-  if ('eid' in out_obj && ['verse', 'chapter'].includes(input_usx_elmt.tagName)) {
+  if ('eid' in outObj && ['verse', 'chapter'].includes(inputUsxElement.tagName)) {
     action = 'ignore';
   }
 
   if (['chapter:c', 'verse:v'].includes(key)) {
-    if ('altnumber' in out_obj) {
-      out_obj = [out_obj];
-      out_obj.push({
+    if ('altnumber' in outObj) {
+      outObj = [outObj];
+      outObj.push({
         'type': `char:${key.slice(-1)}a`,
-        'content': [out_obj[0]['altnumber']],
+        'content': [outObj[0]['altnumber']],
       });
-      delete out_obj[0]['altnumber'];
+      delete outObj[0]['altnumber'];
       action = 'merge';
     }
 
-    if ('pubnumber' in out_obj) {
-      if (!Array.isArray(out_obj)) {
-        out_obj = [out_obj];
+    if ('pubnumber' in outObj) {
+      if (!Array.isArray(outObj)) {
+        outObj = [outObj];
       }
-      out_obj.push({
+      outObj.push({
         'type': `para:${key.slice(-1)}p`,
-        'content': [out_obj[0]['pubnumber']],
+        'content': [outObj[0]['pubnumber']],
       });
-      delete out_obj[0]['pubnumber'];
+      delete outObj[0]['pubnumber'];
       action = 'merge';
     }
   }
 
-  return [out_obj, action];
+  return [outObj, action];
 }
 
-function usx_to_json(input_usx) {
-  let [output_json, _] = convert_usx(input_usx);
-  output_json['type'] = SPEC_NAME;
-  output_json['version'] = VERSION_NUM;
-  return output_json;
+function usxDomToJson(input_usx_dom) {
+  let [outputJson, _] = usxDomToJson1(input_usx_dom);
+  outputJson['type'] = SPEC_NAME;
+  outputJson['version'] = VERSION_NUM;
+  return outputJson;
 }
 
 function main() {
-  let arg_parser = new ArgumentParser({
+  let argParser = new ArgumentParser({
     description: 'Takes in a USX file and converts it into JSON',
   });
 
-  arg_parser.add_argument('infile', {
+  argParser.add_argument('infile', {
     type: String,
     help: 'input USX(.xml) file',
   });
 
-  arg_parser.add_argument('--output_path', {
+  argParser.add_argument('--output_path', {
     type: String,
     help: 'path to write the output file to',
     default: 'STDOUT',
   });
 
-  let args = arg_parser.parse_args();
-  let infile = args.infile;
-  let outpath = args.output_path;
+  let args = argParser.parse_args();
+  let inFile = path.resolve(args.infile);
+  let outPath = args.output_path === 'STDOUT' ? 'STDOUT' : path.resolve(args.output_path);
 
   try {
-    let usx_data = fs.readFileSync(infile, 'utf-8');
+    let usxData = fse.readFileSync(inFile, 'utf-8');
     let parser = new DOMParser();
-    let input_usx = parser.parseFromString(usx_data, 'text/xml');
-    let output_json = usx_to_json(input_usx.documentElement);
-    let json_str = JSON.stringify(output_json, null, 2);
+    let inputUsx = parser.parseFromString(usxData, 'text/xml');
+    let outputJson = usxDomToJson(inputUsx.documentElement);
+    let jsonStr = JSON.stringify(outputJson, null, 2);
 
-    if (outpath === 'STDOUT') {
-      console.log(json_str);
+    if (outPath === 'STDOUT') {
+      console.log(jsonStr);
     } else {
-      fs.writeFileSync(outpath, json_str, 'utf-8');
+      fse.writeFileSync(outPath, jsonStr, 'utf-8');
     }
   } catch (error) {
     console.error('An error occurred:', error.message);
